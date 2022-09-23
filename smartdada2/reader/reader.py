5a28c2a4ca4509505577ac27e36194c0b69b418b
axiomcura <erikishere3@gmail.com>
2022-09-23 14:32:02 -0600

    added documentation and counter

--- smartdada2/reader/reader.py
+++ smartdada2/reader/reader.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from dataclasses import dataclass
+from typing import Sequence
+from typing import Optional
 from typing import Union
 from typing import Optional
-from dataclasses import dataclass
+
 import pandas as pd
 import numpy as np
 
@@ -33,9 +36,20 @@ class FastqReader:
         self.fpath: Path = Path(fpath)
         self.reversed: bool = reverse_seq
         self.technology: str = technology
+        self.n_entries: Optional[int] = 0
+
+        # states
+        self.__counted: bool = False
 
     def get_quality_scores(self) -> pd.DataFrame:
-        """Returns scores in a per sequence bases"""
+        """Returns scores in a per sequence bases
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe containing quality scores per sequence
+
+        """
 
         # converting to np.array
         all_scores = []
@@ -48,38 +62,81 @@ class FastqReader:
         return all_scores
 
     def get_average_score(self) -> pd.Series:
-        """Returns average score of all sequences. Returns a a pd.Series object"""
+        """Returns average score of all sequences. Returns a a pd.Series object
+
+        Returns
+        -------
+        pd.Series
+            average score of all sequences
+
+        """
+
         # get all scores df and take the average per column basis
         scores_df = self.get_quality_scores()
         average_score = scores_df.mean()
         return average_score
 
-    def iter_reads(self):
-        """Returns a python generator containing FastqEntries"""
+    def iter_reads(self) -> Sequence[FastqEntry]:
+        """Returns a python generator containing FastqEntries
+
+        Returns
+        -------
+        Sequence[FastqEntry]
+            Generator object containing FastqEntries
+        """
         return self.__loader()
 
-    def to_list(self):
+    def to_list(self) -> list[FastqEntry]:
         """Saves all lists into memory. Warning, large file sizes will use more
         memory but increase iteration performance.
+
+        Returns
+        -------
+        list[FastqEntry]
+            List of FastqEntries
         """
         return [read for read in self.__loader()]
+
+    def total_reads(self) -> int:
+        """Returns total number of reads. Changes the value of self.n_entries
+        to prevent any recalculation
+
+        Returns
+        -------
+        int
+            total number of reads
+        """
+        # checks if the n_entries have been counted before
+        # -- if not, count and return value
+        # -- if yes, return stored count
+        if self.n_entries == 0 and self.__counted is False:
+            self.n_entries = sum(1 for _ in self.__loader())
+            self.__counted = True
+            return self.n_entries
+        else:
+            return self.n_entries
 
     # ----------------------------------------
     # private functions: users do not interact with this
     # ----------------------------------------
-    def __loader(self):
+    def __loader(self) -> Sequence[FastqEntry]:
         """Creates a generator object that contains sequence read data as
         FastqEntry object.
 
-        Parameters
-        ----------
+        While iterating, a count is also being conducted
+
+        Returns
+        -------
+        Sequence[FastqEntry]
+            Generator object with FastqEntries
         """
 
         # iterate all row contents in fastq file
+        entry_count = 1
         with open(self.fpath, "r") as fastq_file:
 
             contents_chunk = []
-            for idx, row_entry in enumerate(fastq_file):
+            for row_entry in fastq_file:
 
                 # cleaning entries
                 if row_entry == "":
@@ -105,3 +162,5 @@ class FastqReader:
 
                     # yield entry
                     yield fastq_entry
+
+
