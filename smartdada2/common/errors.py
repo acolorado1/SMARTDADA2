8d46f65573e7763dc75ef49ae2084f73936ef92f
axiomcura <erikishere3@gmail.com>
2022-09-24 16:20:51 -0600

    formatting

--- smartdada2/common/errors.py
+++ smartdada2/common/errors.py
@@ -2,13 +2,15 @@
 class __BaseFormatError(Exception):
     """Captures any format related issues"""
 
+
 class __BaseIterationError(Exception):
     pass
+
 
 # Specific Exceptions
 class FastqFormatError(__BaseFormatError):
     """Raised when any format related issues with Fastq files"""
 
-    
+
 class SearchExceededError(__BaseIterationError):
     """Raised if any searching algorithm exceeds max search iteration"""
