22e6942e78611ebc7d629094132f050751ff4906
burkhang <burkhang@MEGAMIND-MAC.local>
2022-11-17 17:40:29 -0700

    creating visualiations for report

---
+++ R_scripts/viz_SizeByAvgEE.R
@@ -0,0 +1,39 @@
+require(readr)
+require(dplyr)
+require(reshape2)
+require(ggplot2)
+
+parameter_info <- read_delim("DADA2ParameterExploration/data/parameter_info.tsv",
+                             delim = "\t", escape_double = FALSE,
+                             col_types = cols(...1 = col_integer(),
+                                              Indexes = col_character(),
+                                              ReadLength = col_integer(),
+                                              ReadsUnderMaxEE = col_integer(),
+                                              ReadsOverMaxEE = col_integer()),
+                             trim_ws = TRUE)
+
+# scatter plot of average expected error per position by read length 
+ggplot(parameter_info, aes(x = ReadLength, y = AvgEEPerPosition)) + 
+  geom_point() + 
+  theme_bw() + 
+  theme(text = element_text(size = 15)) + 
+  ylab('Average Expected Error Per Position') + 
+  xlab('Read Length (bp)') 
+ggsave("DADA2ParameterExploration/plots/ReadLengthByAvgEE.png")
+
+
+# line plot of read counts over and under the maxEE
+melt_param_info <- melt(parameter_info, id.vars = 'ReadLength')
+melt_param_info <- melt_param_info %>% 
+  filter(variable == "ReadsUnderMaxEE" | variable == "ReadsOverMaxEE")
+
+ggplot(melt_param_info, aes(x = ReadLength, y = value)) + 
+  geom_line(aes(color = variable)) +
+  theme_bw() + 
+  theme(text = element_text(size = 20)) + 
+  xlab('Read Length (bp)') + 
+  ylab('Number of reads') + 
+  guides(color=guide_legend(title=" "))
+  
+
+ggsave("DADA2ParameterExploration/plots/ReadCountOverMaxEE.png")
