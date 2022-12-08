2d30cf69f6f0a95d2a0dd404778ac28af96a59ca
axiomcura <erikishere3@gmail.com>
2022-12-08 12:33:27 -0700

    bug fix in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="smartdada2",
-    author="Erik and Sofia"
+    author="Erik and Sofia",
     version="0.0.1",
     description="Dada2 Parameter optimization",
     python_requires=">=3.10",
