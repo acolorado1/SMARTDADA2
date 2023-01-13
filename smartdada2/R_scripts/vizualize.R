bb1d6449562f9cefdf0ebfef088eb3426c0c1def
axiomcura <erikishere3@gmail.com>
2023-01-12 18:19:44 -0700

    formatting with pre-commit

--- smartdada2/R_scripts/vizualize.R
+++ smartdada2/R_scripts/vizualize.R
@@ -10,24 +10,24 @@ parser$add_argument('--sumEE_file', help = 'TSV file containing sum of EE info')
 args <- parser$parse_args()
 
 
-trim_info <- read_delim(args$trim_file, 
-                        delim = "\t", escape_double = FALSE, 
-                        col_types = cols(LeftIndex = col_integer(), 
-                                         RightIndex = col_integer(), ReadLength = col_integer(), 
-                                         ReadsUnderMaxEE = col_integer(), 
-                                         ReadsOverMaxEE = col_integer()), 
+trim_info <- read_delim(args$trim_file,
+                        delim = "\t", escape_double = FALSE,
+                        col_types = cols(LeftIndex = col_integer(),
+                                         RightIndex = col_integer(), ReadLength = col_integer(),
+                                         ReadsUnderMaxEE = col_integer(),
+                                         ReadsOverMaxEE = col_integer()),
                         trim_ws = TRUE)
 
-sumEE_info <- read_delim(args$sumEE_file, 
-                         delim = "\t", escape_double = FALSE, 
+sumEE_info <- read_delim(args$sumEE_file,
+                         delim = "\t", escape_double = FALSE,
                          trim_ws = TRUE)
 
-# plot read length vs quality 
-ggplot(trim_info, aes(ReadLength, AvgEEPerPosition)) + 
-  geom_point() + 
-  theme_bw() + 
-  theme(text = element_text(size = 15)) + 
-  ylab('Average Expected Error Per Position') + 
+# plot read length vs quality
+ggplot(trim_info, aes(ReadLength, AvgEEPerPosition)) +
+  geom_point() +
+  theme_bw() +
+  theme(text = element_text(size = 15)) +
+  ylab('Average Expected Error Per Position') +
   xlab('Read Length (bp)')
 
 ggsave("output/ScatterReadLengthByAvgEE.png",
@@ -36,11 +36,11 @@ ggsave("output/ScatterReadLengthByAvgEE.png",
 
 dev.off()
 
-ggplot(trim_info, aes(LeftIndex, RightIndex, fill= AvgEEPerPosition)) + 
-  geom_tile() + 
+ggplot(trim_info, aes(LeftIndex, RightIndex, fill= AvgEEPerPosition)) +
+  geom_tile() +
   scale_y_continuous(trans = "reverse", breaks = unique(trim_info$RightIndex)) +
   scale_fill_gradient(low="white", high="blue") +
-  theme_bw() 
+  theme_bw()
 
 ggsave("output/HeatmapIndexValueByAvgEE.png",
        width = 10,
@@ -51,12 +51,12 @@ dev.off()
 # read quality by retained reads
 melt_sumEEInfo <- melt(sumEE_info)
 
-ggplot(melt_sumEEInfo, aes(x = value, fill = variable)) + 
-  geom_histogram(position = "dodge") + 
-  theme_bw() + 
+ggplot(melt_sumEEInfo, aes(x = value, fill = variable)) +
+  geom_histogram(position = "dodge") +
+  theme_bw() +
   theme(text = element_text(size = 20)) +
-  scale_x_continuous(limits = c(0,4)) + 
-  guides(fill=guide_legend(title=" "))+ 
+  scale_x_continuous(limits = c(0,4)) +
+  guides(fill=guide_legend(title=" "))+
   xlab('Sum of Expected Error')
 
 ggsave("output/HistogramRetainedReadCount.png",
